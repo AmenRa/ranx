9979b8a316d100c73f5a38bc2f62315b32264930
maximedb <maximedebruyn@gmail.com>
2022-03-24 16:31:44 +0000

    reciprocal rank fusion test

--- tests/unit/ranx/fusion_test.py
+++ tests/unit/ranx/fusion_test.py
@@ -1,6 +1,6 @@
 import pytest
 from ranx.run import Run
-from ranx.fusion import weighted_sum
+from ranx.fusion import weighted_sum, reciprocal_rank_fusion
 import numpy as np
 from numba.typed import List as TypedList
 
@@ -93,3 +93,21 @@ def test_weighted_sum(run_1, run_2, run_3):
         combined_run["q2"]["d3"]
         == weights[1] * run_2["q2"]["d3"] + weights[2] * run_3["q2"]["d3"]
     )
+
+
+def test_reciprocal_rank(run_1, run_2, run_3):
+    combined_run = reciprocal_rank_fusion(
+        TypedList([run_1.run, run_2.run, run_3.run])
+    )
+
+    assert len(combined_run) == 2
+    assert len(combined_run["q1"]) == 3
+    assert len(combined_run["q2"]) == 3
+    
+    assert combined_run["q1"]["d1"] == (1 / (60 + 3)) + (1 / (60 + 1))
+    assert combined_run["q1"]["d2"] == (1 / (60 + 2)) + (1 / (60 + 2))
+    assert combined_run["q1"]["d3"] == (1 / (60 + 1)) + (1 / (60 + 1))
+
+    assert combined_run["q2"]["d1"] == (1 / (60 + 2)) + (1 / (60 + 2))
+    assert combined_run["q2"]["d2"] == (1 / (60 + 1)) + (1 / (60 + 2))
+    assert combined_run["q2"]["d3"] == (1 / (60 + 1)) + (1 / (60 + 1))
