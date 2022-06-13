a6b8fb1cf46431d7559884105d58e8be9f0d2e6d
Elias Bassani <elias.bssn@gmail.com>
2022-06-13 14:17:07 +0200

    Hotfix

--- ranx/__init__.py
+++ ranx/__init__.py
@@ -1,9 +1,12 @@
 from .data_structures import Qrels, Run
-from .meta import compare, evaluate
+from .meta import compare, evaluate, fuse, normalize, optimize_fusion
 
 __all__ = [
     "evaluate",
     "compare",
+    "fuse",
+    "normalize",
+    "optimize_fusion",
     "Qrels",
     "Run",
 ]
